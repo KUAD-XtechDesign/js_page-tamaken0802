1b87f0252203653ef4602c568e4a4b42b437efd5
tamaken0802 <85724739+tamaken0802@users.noreply.github.com>
2021-10-25 00:55:18 +0900

    あ

--- setting.js
+++ setting.js
@@ -1,68 +1,35 @@
 $(function(){
-    //基本設定
- var width = 900;
- var height = 900;
-
- var renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector("#canvas"),
-  alpha: true
- });
- renderer.setPixelRatio(window.devicePixelRatio);
- renderer.setSize(width, height);
- var scene = new THREE.Scene();
-
- // カメラ設定
- var camera = new THREE.PerspectiveCamera(3,width / height,1,10000);
- camera.position.set(1,300, 500); 
+     //基本設定
+     var width = 900;//横幅
+     var height = 900;//縦幅
     
- // ライト設定
- ambientLight = new THREE.AmbientLight(0xffffff);
- hemisphereLight = new THREE.HemisphereLight(0xffffff,0x4169e1, 0.15);
- scene.add(hemisphereLight);
- scene.add(ambientLight);
+     var renderer = new THREE.WebGLRenderer({
+      canvas: document.querySelector("#canvas"),//id="canvas"に描画
+      alpha: true //背景を透過する
+     });
+     renderer.setPixelRatio(window.devicePixelRatio);
+     renderer.setSize(width, height);
+     var scene = new THREE.Scene();
+    
+     // カメラ設定
+     var camera = new THREE.PerspectiveCamera(3,width / height,1,10000);//視野角,縦横比,手前の距離,奥の距離
+     camera.position.set(0, 0, 1000);//カメラ位置
+        
+     // ライト設定
+     ambientLight = new THREE.AmbientLight(0xffffff);//環境光源
+     hemisphereLight = new THREE.HemisphereLight(0xffffff,0x4169e1, 0.5);//半球光源
+     scene.add(hemisphereLight);
+     scene.add(ambientLight);
 
- // ★追加 マウス操作
- var controls = new THREE.OrbitControls(camera);
- controls.enableZoom = false;
- controls.autoRotate = true;
+     // ボックス
+ var geometry = new THREE.BoxGeometry(10, 10, 10);//x,y,z
+ var material = new THREE.MeshStandardMaterial({color: 0xaaaaaa});
+ var box = new THREE.Mesh(geometry, material);
+ scene.add(box);
 
- // ★追加 MTL,Obj読み込み
- new THREE.MTLLoader().setPath('./three/')
- .load('chr_sword.mtl',
- function(materials){
-  materials.preload();
-  new THREE.OBJLoader().setPath('./three/').setMaterials(materials).load('chr_sword.obj',
-  function(object){
-   objmodel = object.clone();
-   obj = new THREE.Object3D();
-   obj.add(objmodel);
-   obj.position.set(0, -1.2, 0);
-   scene.add(obj);        
-  }
-  );
- });
- var obj = new THREE.Mesh();
-
- // 実行
- animate();
- function animate(){
-  requestAnimationFrame(animate);//★追加 アニメーション実行
-  obj.rotation.y += 0.002; //★追加 3Dオブジェクトを自動回転させる
-  renderer.render(scene, camera);
- };
-
- // ★追加 画面リサイズ
- onResize();
- window.addEventListener('resize', onResize);
-
- function onResize() {
-  var width = window.innerWidth;
-  var height = window.innerHeight;
-
-  renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setSize(width, height);
-
-  camera.aspect = width / height;
-  camera.updateProjectionMatrix();
- }
- });
+     // 実行
+     animate();
+     function animate(){
+       renderer.render(scene, camera);
+     };
+    });

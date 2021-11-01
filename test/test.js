16df5ec33adc6823b385670d03777a509b2338b9
tamaken0802 <85724739+tamaken0802@users.noreply.github.com>
2021-11-01 22:27:43 +0900

    a

--- test/test.js
+++ test/test.js
@@ -1,45 +1,106 @@
-window.addEventListener("DOMContentLoaded", init);
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'dat.gui'
 
-function init() {
-  const width = 960;
-  const height = 540;
+// Debug
+const gui = new dat.GUI()
 
-  // レンダラーを作成
-  // レンダラーを作成
-  const renderer = new THREE.WebGLRenderer({
-    canvas: document.querySelector("#myCanvas")
-  });
-  renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setSize(width, height);
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
 
-  // シーンを作成
-  const scene = new THREE.Scene();
+// Scene
+const scene = new THREE.Scene()
 
-  // カメラを作成
-  const camera = new THREE.PerspectiveCamera(
-    45,
-    width / height,
-    1,
-    10000
-  );
-  camera.position.set(0, 0, +1000);
+// Objects
+const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
 
-  // 箱を作成
-  const geometry = new THREE.BoxGeometry(500, 500, 500);
-  const material = new THREE.MeshStandardMaterial({
-    color: 0x0000ff
-  });
-  const box = new THREE.Mesh(geometry, material);
-  scene.add(box);
+// Materials
 
-  // 平行光源
-  const directionalLight = new THREE.DirectionalLight(
-    0xffffff
-  );
-  directionalLight.position.set(1, 1, 1);
-  // シーンに追加
-  scene.add(directionalLight);
+const material = new THREE.MeshBasicMaterial()
+material.color = new THREE.Color(0xff0000)
 
-  // 初回実行
-  renderer.render(scene, camera);
+// Mesh
+const sphere = new THREE.Mesh(geometry,material)
+
+scene.add(sphere)
+
+// Lights
+
+const pointLight = new THREE.PointLight(0xffffff, 0.1)
+pointLight.position.x = 2
+pointLight.position.y = 3
+pointLight.position.z = 4
+scene.add(pointLight)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
 }
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 0
+camera.position.y = 0
+camera.position.z = 2
+scene.add(camera)
+
+// Controls
+// const controls = new OrbitControls(camera, canvas)
+// controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update objects
+    sphere.rotation.y = .5 * elapsedTime
+
+    // Update Orbital Controls
+    // controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
